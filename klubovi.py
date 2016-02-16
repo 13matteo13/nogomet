195aa798070f6ca8bd67a2757db894a88870de60
Matteo Renic <matteo.renic@gmail.com>
2016-02-16 17:31:55 +0100

    sdasldasdeoroq

--- klubovi.py
+++ klubovi.py
@@ -1,2 +1,2 @@
-print('Allahu akbar')
+print('u 4. zadatku pise da upisemo u igraci.py, a ne u klubovi.py tako da..')
 print('Zadatak 5. ja sam vec napisao u sve py datoteke u zadatku 4. Moje iskrene isprike')
