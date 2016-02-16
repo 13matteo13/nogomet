14ab9c94554955e00d71081f028744b72b6b327b
Matteo Renic <matteo.renic@gmail.com>
2016-02-16 17:25:54 +0100

    masha allah 3.0

---
+++ klubovi.py
@@ -0,0 +1,2 @@
+print('Allahu akbar')
+print('Zadatak 5. ja sam vec napisao u sve py datoteke u zadatku 4. Moje iskrene isprike')
--- real/igraci.py
+++ real/igraci.py
@@ -1,1 +1,2 @@
 print('Direktoriji koji se nalaze u direktorij nogomet su:barca,real,juventus i klubovi.py')
+print('Zadatak 5. ja sam vec napisao u sve py datoteke u zadatku 4. Moje iskrene isprike')
